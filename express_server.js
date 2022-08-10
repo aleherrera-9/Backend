13aab91ea34f89146c4a42aeecd203688068d4c2
aleherrera-9 <aleherrera.9.m@gmail.com>
2022-08-10 15:39:01 -0300

    desafio 6

--- express_server.js
+++ express_server.js
@@ -1,20 +1,31 @@
 const express = require('express');
+
 const Container = require('./src/script.js');
 
 const file = new Container();
+
 const app = express();
 
 const PORT = 8080;
 
-app.get('/productos', async(req,res) => {
-  const products=await file.getAll();
-  res.send(products);
+function getRandomArbitrary(min, max) {
+    let seeder = Math.random();
+    let numAleatorio = parseInt(seeder * max + min);
+    return numAleatorio;
+}
+
+app.get('/productos', async (req, res) => {
+    const products = await file.getAll();
+    size = products.length;
+    res.send(products);
 })
 
-// app.get('/productoRandom', (req, res) => {
-
-//     res.send();
-// })
+app.get('/productoRandom', async (req, res) => {
+    const products = await file.getAll();
+    size = products.length;
+    let id = getRandomArbitrary(1, size);
+    res.send(`${await file.getById(id)}`);
+})
 
 app.get('*', (req, res) => {
     res.send('404-not found');
