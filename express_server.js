f12ec77645c030b703ead617e63bb056fd131052
aleherrera-9 <aleherrera.9.m@gmail.com>
2022-08-10 15:45:42 -0300

    updated

--- express_server.js
+++ express_server.js
@@ -5,26 +5,24 @@ const Container = require('./src/script.js');
 const file = new Container();
 
 const app = express();
-
+let size=0;
 const PORT = 8080;
-
-function getRandomArbitrary(min, max) {
-    let seeder = Math.random();
-    let numAleatorio = parseInt(seeder * max + min);
+  function getRandomArbitrary(min,max){
+    let seeder=Math.random();
+    let numAleatorio=parseInt(seeder*max+min);
     return numAleatorio;
 }
-
-app.get('/productos', async (req, res) => {
-    const products = await file.getAll();
-    size = products.length;
-    res.send(products);
+app.get('/productos', async(req,res) => {
+  const products=await file.getAll();
+  size=products.length;
+  res.send(products);
 })
 
-app.get('/productoRandom', async (req, res) => {
-    const products = await file.getAll();
-    size = products.length;
-    let id = getRandomArbitrary(1, size);
-    res.send(`${await file.getById(id)}`);
+ app.get('/productoRandom',async(req, res) => {
+    const products=await file.getAll();
+    size=products.length;
+    let id=getRandomArbitrary(1,size);
+   res.send(`${await file.getById(id)}`);
 })
 
 app.get('*', (req, res) => {
