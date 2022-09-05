1c7075c3c57e06302609440104e5a0029624208d
aleherrera-9 <aleherrera.9.m@gmail.com>
2022-09-05 16:04:59 -0300

    new sv

---
+++ src/script.js
@@ -0,0 +1,34 @@
+const fs = require('fs/promises');
+
+class Container {
+    constructor() { }
+
+    async getAll() {
+        try {
+            const files = await fs.readFile('./DB_HBS/list.txt', 'utf-8');
+            return (JSON.parse(files));
+        } catch (error) {
+            return []
+        }
+    }
+  
+    async save(obj) {
+        try {
+            const allFiles = await this.getAll();
+            let newId;
+            if (allFiles.length == 0) {
+                newId = 1
+            }
+            else {
+                newId = allFiles[allFiles.length - 1].id + 1
+            }
+            const newFile = { id: newId, ...obj }
+            allFiles.push(newFile)
+            await fs.writeFile('./DB_HBS/list.txt', JSON.stringify(allFiles, null, 2));
+        } catch (error) {
+            return 'error al guardar'
+        }
+    }
+   
+}
+module.exports = Container;
